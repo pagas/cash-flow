9229e55f1eec87e52246d98f91cc7c9d5aa462d0
pagas <paulius.gaida@gmail.com>
2017-03-01 00:32:41 +0000

    request for a customer form quickbook

--- example/app.js
+++ example/app.js
@@ -5,11 +5,13 @@ var http       = require('http'),
     util       = require('util'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
-    session    = require('express-session'),
+    //session    = require('express-session'),
+    cookieSession = require('cookie-session'),
     express    = require('express'),
     app        = express(),
     QuickBooks = require('../index')
 
+require('dotenv').config();
 
 // Generic Express config
 app.set('port', port)
@@ -17,16 +19,18 @@ app.set('views', 'views')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser('brad'))
-app.use(session({resave: false, saveUninitialized: false, secret: 'smith'}));
+//app.use(session({resave: false, saveUninitialized: false, secret: 'smith'}));
+app.use(cookieSession({
+  name: 'session', keys:['key1']
+}));
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'))
 })
 
 // INSERT YOUR CONSUMER_KEY AND CONSUMER_SECRET HERE
-
-var consumerKey    = '',
-    consumerSecret = ''
+var consumerKey    = process.env.CUSTOMER_KEY,
+consumerSecret = process.env.CUSTOMER_SECRET;
 
 app.get('/',function(req,res){
   res.redirect('/start');
@@ -35,6 +39,13 @@ app.get('/',function(req,res){
 app.get('/start', function(req, res) {
   res.render('intuit.ejs', {locals: {port:port, appCenter: QuickBooks.APP_CENTER_BASE}})
 })
+
+app.get('/customer/:id', function(req, res) {
+  var qbo = getQbo(req.session.qbo);
+  qbo.getCustomer(req.params.id, function(err, customer) {
+    res.render('customer.ejs', { locals: { customer: customer }})
+  });
+});
 
 app.get('/requestToken', function(req, res) {
   var postBody = {
@@ -48,7 +59,7 @@ app.get('/requestToken', function(req, res) {
   request.post(postBody, function (e, r, data) {
     var requestToken = qs.parse(data)
     req.session.oauth_token_secret = requestToken.oauth_token_secret
-    console.log(requestToken)
+    //console.log(requestToken)
     res.redirect(QuickBooks.APP_CENTER_URL + requestToken.oauth_token)
   })
 })
@@ -67,25 +78,35 @@ app.get('/callback', function(req, res) {
   }
   request.post(postBody, function (e, r, data) {
     var accessToken = qs.parse(data)
-    console.log(accessToken)
-    console.log(postBody.oauth.realmId)
+    //console.log(accessToken)
+    //  console.log(postBody.oauth.realmId)
 
-    // save the access token somewhere on behalf of the logged in user
-    qbo = new QuickBooks(consumerKey,
-                         consumerSecret,
-                         accessToken.oauth_token,
-                         accessToken.oauth_token_secret,
-                         postBody.oauth.realmId,
-                         true, // use the Sandbox
-                         true); // turn debugging on
+    req.session.qbo = {
+      token: accessToken.oauth_token,
+      secret: accessToken.oauth_token_secret,
+      companyId: postBody.oauth.realmId
+    };
+
+    qbo = getQbo(req.session.qbo);
 
     // test out account access
     qbo.findAccounts(function(_, accounts) {
       accounts.QueryResponse.Account.forEach(function(account) {
-        console.log(account.Name)
+        //console.log(account.Name)
       })
     })
+    res.send('<!DOCTYPE html><html lang="en"><head></head><body><script>window.opener.location.reload(); window.close();</script></body></html>')
   })
-  res.send('<!DOCTYPE html><html lang="en"><head></head><body><script>window.opener.location.reload(); window.close();</script></body></html>')
+
 })
 
+var getQbo = function(args) {
+  // save the access token somewhere on behalf of the logged in user
+  return new QuickBooks(consumerKey,
+                       consumerSecret,
+                       args.token,
+                       args.secret,
+                       args.companyId,
+                       true, // use the Sandbox
+                       true); // turn debugging on
+}
